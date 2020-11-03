3c712603adaf071112702667cede65290d0cf608
Brendan Sailer <bsailer1@nd.edu>
2020-11-03 15:09:43 -0500

    Add basic socket implementation to connect client and server

--- pong_c/netpong.c
+++ pong_c/netpong.c
@@ -4,11 +4,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 #define WIDTH 43
 #define HEIGHT 21
 #define PADLX 1
 #define PADRX WIDTH - 2
+
+int setup_host(int);
+int setup_client(char*, int);
+void send_int(int, int);
+int recv_int(int);
 
 // Global variables recording the state of the game
 // Position of ball
@@ -21,6 +30,8 @@ int padLY, padRY;
 int scoreL, scoreR;
 // Current Round
 int curr_round, num_rounds;
+// Determines if the program is the host
+int isHost = 0;
 
 // ncurses window
 WINDOW *win;
@@ -151,9 +162,9 @@ void tock() {
 		if(scoreR == 2){
 			char message[BUFSIZ];
 			sprintf(message, "Round %d", curr_round);
-        	countdown(message, "WIN -->");
+					countdown(message, "WIN -->");
 		} else{
-        	countdown("SCORE -->", " ");
+					countdown("SCORE -->", " ");
 		}
     } else if(ballX == WIDTH - 1) {
         scoreL = (scoreL + 1) % 100;
@@ -161,9 +172,9 @@ void tock() {
 		if(scoreL == 2){
 			char message[BUFSIZ];
 			sprintf(message, "Round %d", curr_round);
-        	countdown(message, "<-- WIN");
+					countdown(message, "<-- WIN");
 		} else{
-        	countdown("<-- SCORE", " ");
+					countdown("<-- SCORE", " ");
 		}
     }
 
@@ -175,20 +186,26 @@ void tock() {
  * Updates global pad positions
  */
 void *listenInput(void *args) {
-    while(curr_round <= num_rounds) {
-        switch(getch()) {
-            case KEY_UP: padRY--;
-             break;
-            case KEY_DOWN: padRY++;
-             break;
-            case 'w': padLY--;
-             break;
-            case 's': padLY++;
-             break;
-            default: break;
+	while(curr_round <= num_rounds) {
+		if(isHost){ // Host uses arrow keys
+			switch(getch()) {
+				case KEY_UP: padRY--;
+					break;
+				case KEY_DOWN: padRY++;
+					break;
+				default: break;
+			}
+		} else { // Client uses w and s keys
+			switch(getch()) {
+				case 'w': padLY--;
+					break;
+				case 's': padLY++;
+					break;
+				default: break;
        }
-    }       
-    return NULL;
+    }
+	}
+	return NULL;
 }
 
 void initNcurses() {
@@ -205,80 +222,212 @@ void initNcurses() {
 }
 
 int main(int argc, char *argv[]) {
-    // Process args
-    // refresh is clock rate in microseconds
-    // This corresponds to the movement speed of the ball
-		/*
-		char *host;
-		int port;
-		int isHost = 0;
-		if(argc != 3){
-			printf("Useage: ./netpong --host PORT\n");
-			printf("Useage: ./netpong HOSTNAME PORT\n");
-			exit(1);
-		} else {
-			if(strcmp(argv[1], "--host") == 0){
-				isHost = 1;
-			} else{
-				host = argv[1];
-			}
-			port = atoi(argv[2]);
+	// Process args
+	// refresh is clock rate in microseconds
+	// This corresponds to the movement speed of the ball
+		
+	char *host;
+	int port;
+	if(argc != 3){
+		printf("Useage: ./netpong --host PORT\n");
+		printf("Useage: ./netpong HOSTNAME PORT\n");
+		exit(1);
+	} else {
+		if(strcmp(argv[1], "--host") == 0){
+			isHost = 1;
+		} else{
+			host = argv[1];
 		}
-		printf("isHost %d, port: %d, host: %s\n", isHost, port, host);
-		*/
+		port = atoi(argv[2]);
+	}
+	printf("isHost %d, port: %d, host: %s\n", isHost, port, host);
 
-    int refresh;
-    char difficulty[10]; 
+	int refresh, socket;
+	char difficulty[10]; 
 	char rounds[10];
-    printf("Please select the difficulty level (easy, medium or hard): ");
-    scanf("%s", &difficulty);
-	printf("Please enter the maximum number of rounds to play: ");
-	scanf("%s", &rounds);
-	num_rounds = atoi(rounds);
-
-	while(num_rounds <= 0){
-		printf("Please enter the maximum number of rounds to play (must be greater than 0): ");
+	if(isHost){ // Only let the host pick the difficulty and number of rounds
+		printf("Please select the difficulty level (easy, medium or hard): ");
+		scanf("%s", &difficulty);
+		printf("Please enter the maximum number of rounds to play: ");
 		scanf("%s", &rounds);
 		num_rounds = atoi(rounds);
+
+		while(num_rounds <= 0){
+			printf("Please enter the maximum number of rounds to play (must be greater than 0): ");
+			scanf("%s", &rounds);
+			num_rounds = atoi(rounds);
+		}
+
+		// Setup host connection
+		socket = setup_host(port);
+		if(strcmp(difficulty, "easy") == 0) refresh = 80000;
+		else if(strcmp(difficulty, "medium") == 0) refresh = 40000;
+		else if(strcmp(difficulty, "hard") == 0) refresh = 20000;
+		else refresh = 80000;
+	
+		// Send the refresh and the number of rounds to the client
+		send_int(socket, refresh);
+		int ack = recv_int(socket);
+		send_int(socket, num_rounds);
+	} else {
+		// Connect to the host
+		socket = setup_client(host, port);
+
+		// Receive the refresh and the number of rounds from the host
+		refresh = recv_int(socket);
+		send_int(socket, 0);
+		num_rounds = recv_int(socket);
 	}
+	printf("setup complete!\n");
 
 	curr_round = 1;
-	
-    if(strcmp(difficulty, "easy") == 0) refresh = 80000;
-    else if(strcmp(difficulty, "medium") == 0) refresh = 40000;
-    else if(strcmp(difficulty, "hard") == 0) refresh = 20000;
 
-    // Set up ncurses environment
-    initNcurses();
+	// Set up ncurses environment
+	initNcurses();
 
-    // Set starting game state and display a countdown
-    reset();
-    countdown("Starting Game", " ");
+	// Set starting game state and display a countdown
+	reset();
+	countdown("Starting Game", " ");
     
-    // Listen to keyboard input in a background thread
-    pthread_t pth;
-    pthread_create(&pth, NULL, listenInput, NULL);
+	// Listen to keyboard input in a background thread
+	pthread_t pth;
+	pthread_create(&pth, NULL, listenInput, NULL);
 
-    // Main game loop executes tock() method every REFRESH microseconds
-    struct timeval tv;
-    while(curr_round <= num_rounds) {
-        gettimeofday(&tv,NULL);
-        unsigned long before = 1000000 * tv.tv_sec + tv.tv_usec;
-        tock(); // Update game state
-        gettimeofday(&tv,NULL);
-        unsigned long after = 1000000 * tv.tv_sec + tv.tv_usec;
-        unsigned long toSleep = refresh - (after - before);
-        // toSleep can sometimes be > refresh, e.g. countdown() is called during tock()
-        // In that case it's MUCH bigger because of overflow!
-        if(toSleep > refresh) toSleep = refresh;
-        usleep(toSleep); // Sleep exactly as much as is necessary
-    }
+	// Main game loop executes tock() method every REFRESH microseconds
+	struct timeval tv;
+	while(curr_round <= num_rounds) {
+		gettimeofday(&tv,NULL);
+		unsigned long before = 1000000 * tv.tv_sec + tv.tv_usec;
+		tock(); // Update game state
+		gettimeofday(&tv,NULL);
+		unsigned long after = 1000000 * tv.tv_sec + tv.tv_usec;
+		unsigned long toSleep = refresh - (after - before);
+		// toSleep can sometimes be > refresh, e.g. countdown() is called during tock()
+		// In that case it's MUCH bigger because of overflow!
+		if(toSleep > refresh) toSleep = refresh;
+		usleep(toSleep); // Sleep exactly as much as is necessary
+	}
 
 	//Game ended print
 	game_over();
  
-    // Clean up
-    pthread_join(pth, NULL);
-    endwin();
-    return 0;
+	// Clean up
+	pthread_join(pth, NULL);
+	endwin();
+	return 0;
 }
+
+int setup_host(int port){
+	struct sockaddr_in sin, client_addr;
+	char buf[BUFSIZ];
+	int len, addr_len;
+	int s, new_s;
+
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(port);
+
+	/* setup passive open */
+	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("simplex-talk: socket");
+		exit(1);
+	}
+
+	// set socket option
+	int opt = 1;
+	if((setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)& opt, sizeof(int)))<0){
+		perror ("netpong: setscokt");
+		exit(1);
+	}
+
+	if((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+		perror("netpong: bind"); exit(1);
+	}
+	if((listen(s, 2))<0){
+		perror("netpong: listen"); exit(1);
+	}
+
+	printf("Waiting for connections...\n");
+	/* wait for connection, then receive and print text */
+	addr_len = sizeof (client_addr);
+	if((new_s = accept(s, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
+		perror("netpong: accept");
+		exit(1);
+	}
+
+	close(s);
+	return new_s;
+}
+
+int setup_client(char* host, int port){
+	struct hostent *hp;
+	struct sockaddr_in sin;
+	char buf[BUFSIZ];
+	char reply[BUFSIZ];
+	int s;
+
+	/* translate host name into peer's IP address */
+	hp = gethostbyname(host);
+	if (!hp) {
+		fprintf(stderr, "netpong: unknown host: %s\n", host);
+		exit(1);
+	}
+	
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+	sin.sin_port = htons(port);
+
+	/* active open */
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("netpong: socket");
+		exit(1);
+	}
+
+	if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0){
+		perror("netpong: connect");
+		close(s);
+		exit(1);
+	}
+
+	return s;
+}
+
+void send_int(int socket, int value){
+	int formatted = htonl(value);
+	if(send(socket, &formatted, sizeof(formatted), 0)==-1){
+		perror("netpong: error sending message");
+		exit(1);
+	}
+}
+
+int recv_int(int socket){
+	int length;
+	int received_int;
+	if((length=recv(socket, &received_int, sizeof(received_int), 0)) < 0){
+		perror("netpong: error receiving reply");
+		exit(1);
+	}
+	return ntohl(received_int);
+}
+
+/*
+		printf("Connection established.\n");
+		while (1){
+			if((len=recv(new_s, buf, sizeof(buf), 0))==-1){
+				perror("Server Received Error!");
+				exit(1);
+			}
+			else if(len==0){
+				break;
+			}
+			complete_request(new_s, buf);
+			bzero((char *) &buf, sizeof(buf));
+		}
+
+		printf("Client finishes, close the connection!\n");
+		close(new_s);
+*/
